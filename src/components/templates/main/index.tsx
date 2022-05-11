5da25274c6a6e90e993d3aedc406818f105453e1
Rafael Fagundes <rafaelcflima@gmail.com>
2022-05-10 22:40:28 -0300

    Type definitions and typo corrections

--- src/components/templates/main/index.tsx
+++ src/components/templates/main/index.tsx
@@ -1,13 +1,19 @@
 import { Box } from "@chakra-ui/react";
 import Head from "next/head";
+import { Router } from "next/router";
 import React from "react";
 
-function Main({ children, router }) {
+type MainProps = {
+  children: JSX.Element | JSX.Element[];
+  router?: Router;
+};
+
+function Main({ children }: MainProps) {
   return (
     <Box as="main">
       <Head>
         <meta name="viweport" content="width=device-width, initial-scale=1" />
-        <title>Rafael Fagundes - Portifolio Website</title>
+        <title>Rafael Fagundes - Portfolio Website</title>
       </Head>
       {children}
     </Box>
